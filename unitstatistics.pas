af1a9f15f3461f8a0af20a929fb83b0a358cdeee
Nico Straube <74559993+NicoStraube@users.noreply.github.com>
2021-02-18 14:34:13 +0100

    Create unitstatistics.pas

---
+++ unitstatistics.pas
@@ -0,0 +1,27 @@
+unit unitStatistics;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TformStatistics = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  formStatistics: TformStatistics;
+
+implementation
+
+uses unitMain;
+
+{$R *.lfm}
+
+end.
